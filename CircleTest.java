8913d83a9a06d1eaf114f4a403519db6df58b34d
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-02 14:40:24 +0700

    pf-java-oop Cricle

--- CircleTest.java
+++ CircleTest.java
@@ -1,5 +1,10 @@
 public class CircleTest {
     public static void main(String[] args) {
-
+//        Circle circle = new Circle(1.0 ,"green");
+//        System.out.println(circle);
+//        System.out.println("Area Circle is: "+circle.getArea());
+        Cylinder cylinder = new Cylinder(5.0,"red",2.0);
+        System.out.println(cylinder);
+        System.out.println("The volume Cylinder:"+cylinder.getVolume());
     }
 }
--- Cylinder.java
+++ Cylinder.java
@@ -1,2 +1,39 @@
-public class Cylinder {
+public class Cylinder extends Circle {
+    private double height = 2.0;
+
+    public Cylinder() {
+
+    }
+
+    public Cylinder(double height) {
+        this.height = height;
+    }
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return Math.PI * height * getRadius();
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" + "\tradius= "
+                + getRadius()
+                + "\tcolor= "
+                + getHeight()
+                + "\theight= "
+                + height +
+                '}';
+    }
 }
